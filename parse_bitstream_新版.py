05bb2a57a0339edfc98669d7d9c2521ded65df54
ZhaoShuai <dsupermaker@163.com>
2024-10-26 18:22:47 +0800

    完整整个码流的读取

--- parse_bitstream_新版.py
+++ parse_bitstream_新版.py
@@ -9,6 +9,13 @@ logging.basicConfig(level=logging.DEBUG)  # 调试模式
 
 file_path = r"E:\CodeSpace_vscode\P_筛片脚本\xilinx_pcie_2_1_rport_7x_xilinx.bit"
 
+def bytes_to_binary(byte_data):
+    if len(byte_data) < 4:
+        # 不足 4 个字节
+        logging.warning(f"Warning: Received less than 4 bytes. Padding with zeros to right.")
+        byte_data = byte_data.rjust(4, b'\x00')  # 用 0 补齐到 4 字节
+    return ''.join(f'{byte:08b}' for byte in byte_data)
+
 def show_ascii_content(content):
     print_content = ""
     for byte in content:
@@ -75,11 +82,11 @@ class ConfigurationPacket:
         return word >> 29 
 
     def get_type_1_packet_content(self, word):
-        header_type = self.get_packet_type(word)
-        opcode = self.OpCode((word >> 27) & 0x3)
-        address = self.Address((word >> 13) & 0x1F)
-        reserved = (word >> 11) & 0x3
-        word_count = word & 0x7FF
+        header_type = self.get_packet_type(word) # [31:29]
+        opcode = self.OpCode((word >> 27) & 0x3) # [28:27]
+        address = self.Address((word >> 13) & 0x1F) # [26:13]
+        reserved = (word >> 11) & 0x3 # [12:11]
+        word_count = word & 0x7FF # [10:0]
         return {
                 "header_type":header_type,
                 "opcode":opcode,
@@ -87,7 +94,17 @@ class ConfigurationPacket:
                 "reserved":reserved,
                 "word_count":word_count
             }
-    
+        
+    def get_type_2_packet_content(self, word):
+        header_type = self.get_packet_type(word) # [31:29]
+        opcode = self.OpCode((word >> 27) & 0x3) # [28:27]
+        word_count = word & 0x7FFFFFF # [26:0]
+        return {
+                "header_type":header_type,
+                "opcode":opcode,
+                "word_count":word_count
+            }
+        
     def __init__(self) -> None:
         pass
     
@@ -106,8 +123,10 @@ class BitstreamReader:
         # 读取位置
         self.byte_content_cur_loc = 0
         
+        # 数据内容的字节长度
+        self.bit_data_content_byte_count = 0
         # 存储头部信息，FF之前
-        self.bit_head_content = b''
+        self.bit_head_byte_content = b''
          # 存储头部配置信息
         self.bit_cfg_content_pre = []
         # 存储码流主体内容
@@ -137,77 +156,78 @@ class BitstreamReader:
         end_index = 0
         
         # ========== 起始数据 ============
-        self.bit_head_content += self.read_bytes(2) # 标识
-        self.bit_head_content += self.read_bytes(11) # 内容
+        self.bit_head_byte_content += self.read_bytes(2) # 标识
+        self.bit_head_byte_content += self.read_bytes(11) # 内容
         start_index += 13
         # ========== 起始数据 ============
         
         # ========== 标识符A ============
-        self.bit_head_content += self.read_bytes(1) # 标识 b'\x61'
+        self.bit_head_byte_content += self.read_bytes(1) # 标识 b'\x61'
         cur_group_length = self.read_bytes(2)
-        self.bit_head_content += cur_group_length  # 内容 该组字节长度
+        self.bit_head_byte_content += cur_group_length  # 内容 该组字节长度
         group_length = struct.unpack('>H', cur_group_length)[0]
-        self.bit_head_content += self.read_bytes(group_length) # 文件路径、Version、UserID
+        self.bit_head_byte_content += self.read_bytes(group_length) # 文件路径、Version、UserID
         start_index += 3
         end_index = start_index + group_length
-        logging.debug(f"\tA content: {self.bit_head_content[start_index:end_index].hex()}")
-        show_ascii_content(self.bit_head_content[start_index:end_index])
+        logging.debug(f"\tA content: {self.bit_head_byte_content[start_index:end_index].hex()}")
+        show_ascii_content(self.bit_head_byte_content[start_index:end_index])
         start_index = end_index
         # ========== 标识符A ============
         
         # ========== 标识符B ============
-        self.bit_head_content += self.read_bytes(1) # 标识 b'\x62'
+        self.bit_head_byte_content += self.read_bytes(1) # 标识 b'\x62'
         cur_group_length = self.read_bytes(2)
-        self.bit_head_content += cur_group_length  # 内容 该组字节长度
+        self.bit_head_byte_content += cur_group_length  # 内容 该组字节长度
         group_length = struct.unpack('>H', cur_group_length)[0]
-        self.bit_head_content += self.read_bytes(group_length) # part name
+        self.bit_head_byte_content += self.read_bytes(group_length) # part name
         start_index += 3
         end_index = start_index + group_length
-        logging.debug(f"\tB content: {self.bit_head_content[start_index:end_index].hex()}")
-        show_ascii_content(self.bit_head_content[start_index:end_index])
+        logging.debug(f"\tB content: {self.bit_head_byte_content[start_index:end_index].hex()}")
+        show_ascii_content(self.bit_head_byte_content[start_index:end_index])
         start_index = end_index
         # ========== 标识符B ============
         
         # ========== 标识符C ============
-        self.bit_head_content += self.read_bytes(1) # 标识 b'\x63'
+        self.bit_head_byte_content += self.read_bytes(1) # 标识 b'\x63'
         cur_group_length = self.read_bytes(2)
-        self.bit_head_content += cur_group_length  # 内容 该组字节长度
+        self.bit_head_byte_content += cur_group_length  # 内容 该组字节长度
         group_length = struct.unpack('>H', cur_group_length)[0]
-        self.bit_head_content += self.read_bytes(group_length) # 年/月/日
+        self.bit_head_byte_content += self.read_bytes(group_length) # 年/月/日
         start_index += 3
         end_index = start_index + group_length
-        logging.debug(f"\tC content: {self.bit_head_content[start_index:end_index].hex()}")
-        show_ascii_content(self.bit_head_content[start_index:end_index])
+        logging.debug(f"\tC content: {self.bit_head_byte_content[start_index:end_index].hex()}")
+        show_ascii_content(self.bit_head_byte_content[start_index:end_index])
         start_index = end_index
         # ========== 标识符C ============
 
         # ========== 标识符D ============
-        self.bit_head_content += self.read_bytes(1) # 标识 b'\x64'
+        self.bit_head_byte_content += self.read_bytes(1) # 标识 b'\x64'
         cur_group_length = self.read_bytes(2)
-        self.bit_head_content += cur_group_length  # 内容 该组字节长度
+        self.bit_head_byte_content += cur_group_length  # 内容 该组字节长度
         group_length = struct.unpack('>H', cur_group_length)[0]
-        self.bit_head_content += self.read_bytes(group_length) # 时:分:秒
+        self.bit_head_byte_content += self.read_bytes(group_length) # 时:分:秒
         start_index += 3
         end_index = start_index + group_length
-        logging.debug(f"\tD content: {self.bit_head_content[start_index:end_index].hex()}")
-        show_ascii_content(self.bit_head_content[start_index:end_index])
+        logging.debug(f"\tD content: {self.bit_head_byte_content[start_index:end_index].hex()}")
+        show_ascii_content(self.bit_head_byte_content[start_index:end_index])
         start_index = end_index
         # ========== 标识符D ============
 
         # ========== 标识符E ============
-        self.bit_head_content += self.read_bytes(1) # 标识 b'\x65'
-        self.bit_head_content += self.read_bytes(4) # 位流的总长度
+        self.bit_head_byte_content += self.read_bytes(1) # 标识 b'\x65'
+        self.bit_head_byte_content += self.read_bytes(4) # 位流的总长度
         start_index += 1
         end_index = start_index + 4
-        logging.debug(f"\tE content: {self.bit_head_content[start_index:end_index].hex()}")
-        show_number_content(self.bit_head_content[start_index:end_index])
+        logging.debug(f"\tE content: {self.bit_head_byte_content[start_index:end_index].hex()}")
+        show_number_content(self.bit_head_byte_content[start_index:end_index])
         start_index = end_index
         # ========== 标识符E ============
             
     # 解析位流，读取cfg内容
-    def parse_cfg_content(self) -> None: 
+    def parse_cfg_content_pre(self) -> None: 
         while True:
             word = self.read_bytes(4)
+            print(bytes_to_binary(word))
             if not word:
                 break  # 到达文件末尾，停止读取
             if len(word) < 4:
@@ -218,52 +238,89 @@ class BitstreamReader:
             self.bit_cfg_content_pre.append(word)
             word_content = struct.unpack('>I', word)[0] # 转无符号整型
             
-            
-            if($line =~ /^1{32,}/ or $line =~ /^00111/){
-                $comment="DUMMY";
-            }elsif($line =~ /^10101010100110010101010101100110/){
-                $comment = "SYNC WORD";
-            }elsif($line =~ /^00000000000000000000000010111011/ or $line =~ /^00010001001000100000000001000100/){
-                $comment = "BUS WIDTH AUTO DETECT";
-            }elsif($line =~ /^00100/){
-                $comment="NOP";
-        
-        
-            
+            if word_content == 0b11111111111111111111111111111111:
+                # DUMMY
+                continue
+            elif word_content == 0b10101010100110010101010101100110:
+                # SYNC WORD
+                continue
+            elif word_content == 0b00000000000000000000000010111011 or word_content == 0b00010001001000100000000001000100:
+                # BUS WIDTH AUTO DETECT
+                continue
+            elif word_content == 0b00100000000000000000000000000000:
+                # NOP
+                continue
             
             content = self.cfg_obj.get_type_1_packet_content(word_content)
-            # 读取到FDRI后结束
+            # 读取到 FDRI 后，读取结束 30004000
             if content.get("header_type", -1) == 1 \
                 and content.get("opcode", self.cfg_obj.OpCode.UNKNOWN) == self.cfg_obj.OpCode.WRITE \
                 and content.get("address", self.cfg_obj.Address.UNKNOWN) == self.cfg_obj.Address.FDRI:
+                    word = self.read_bytes(4)
+                    self.bit_cfg_content_pre.append(word)
+                    word_content = struct.unpack('>I', word)[0] # 转无符号整型
+                    # 拿到 word_count，其单位是word，换成字节*4
+                    self.word_count = self.cfg_obj.get_type_2_packet_content(word_content).get("word_count",0)
+                    self.bit_data_content_byte_count = self.word_count * 4
                     break
+               
+    # 解析位流，读取cfg内容
+    def parse_cfg_content_aft(self) -> None: 
+        while True:
+            word = self.read_bytes(4)
+            if not word:
+                break  # 到达文件末尾，停止读取
+            if len(word) < 4:
+                # 如果最后的 chunk 不足 4 个字节，输出提示
+                logging.warning(f"Warning: Last chunk is less than 4 bytes: {word.hex()}")
+            
+            # 存入cfg中
+            self.bit_cfg_content_after.append(word)
         
+    def parse_data_content(self) -> None: 
+        data_content = self.read_bytes(self.bit_data_content_byte_count)
+        for i in range(0,self.bit_data_content_byte_count,4):
+            self.bit_data_content.append(data_content[i:i+4])
+    
     def read_bit_or_bin_file_and_parse(self, file_type: str) -> None:
         # ============================================ bit header ============================================
         if file_type == "bit":
             self.parse_head_content()
         # ============================================ bit header ============================================
         
-        # ============================================ cfg content ============================================
-        self.parse_cfg_content()
-        # ============================================ cfg content ============================================
-        
-        
+        # ============================================ cfg content pre ============================================
+        # 从 FFFFFFFF 开始
+        self.parse_cfg_content_pre()
+        # 到 30004000 XXXXXXXX 结束，其中 XXXXXXXX 的低27位标识接下来 data frame 的长度
+        # self.word_count 为 接下来有多少个 word
+        # self.bit_data_content_byte_count 为 接下来有多少个字节
+        # 1个word为4字节，1字节为8位
+        # ============================================ cfg content pre ============================================
+                
+        # ============================================ data frame ============================================
+        # 从 FDRI data word 1 开始
+        self.parse_data_content()
+        # 到 FDRI data word XXXX 结束，其中 XXXX 指的是 parse_cfg_content_pre 解析出来的 self.word_count
         # ============================================ data frame ============================================
         
+        # ============================================ cfg content after ============================================
+        # 对于没有关闭CRC的位流，此处从 30000001 开始
+        self.parse_cfg_content_aft()
+        # 到 码流末尾 结束
+        # ============================================ cfg content after ============================================
         
-        # ============================================ data frame ============================================
+        # len(self.bit_cfg_content_pre) + len(self.bit_data_content) + len(self.bit_cfg_content_after) == 总word数量
         
-        # ========== 读取剩余内容 ============
-        while True:
-            chunk = self.read_bytes(4)
-            if not chunk:
-                break  # 到达文件末尾，停止读取
-            if len(chunk) < 4:
-                # 如果最后的 chunk 不足 4 个字节，输出提示
-                logging.warning(f"Warning: Last chunk is less than 4 bytes: {chunk.hex()}")
-            self.bit_data_content.append(chunk)
-        # ========== 读取剩余内容 ============
+        # len(self.bit_head_byte_content)) 头部信息字节数
+        # len(self.bit_cfg_content_pre)) 数据帧之前的寄存器所占word数，*4为字节数
+        # len(self.bit_data_content)) 数据帧所占word数，*4为字节数
+        # len(self.bit_cfg_content_after)) 数据帧之后的寄存器所占word数，*4为字节数
+        # 四个字节数相加为整段位流长度
+        print(format(len(self.bit_head_byte_content), 'X'))
+        print(format(len(self.bit_cfg_content_pre)*4, 'X'))
+        print(format(len(self.bit_data_content)*4, 'X'))
+        print(format(len(self.bit_cfg_content_after)*4, 'X'))
+        print(len(self.bit_cfg_content_after)*4 + len(self.bit_data_content)*4 + len(self.bit_cfg_content_pre)*4 + len(self.bit_head_byte_content))
     
     def read_bytes(self, read_length: int) -> bytes:
         """
@@ -289,7 +346,6 @@ class BitstreamReader:
         data = self.byte_content[self.byte_content_cur_loc : self.byte_content_cur_loc + read_length]
         self.byte_content_cur_loc += read_length
         return data
-    
     
 INPUT_FILE_PATH = r"E:\workspace\parse_bitstream\parse_bitstream\data\system_wrapper_1_原版.bit"
 # 打开文件，以二进制模式('rb')读取
